2c214419751a86a2d4f2a07b35051b862d5e8570
Vitória Anjos <anjosvitoria80@gmail.com>
2026-02-10 00:10:25 -0300

    add entitys

--- src/main/java/com/eventostec/api/domain/coupom/Coupom.java
+++ src/main/java/com/eventostec/api/domain/coupom/Coupom.java
@@ -1,5 +1,34 @@
 package com.eventostec.api.domain.coupom;
 
+import com.eventostec.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name="event-coupon")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Coupom {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+    private Integer discount;
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name="event_id")
+    private Event event;
+
+
 }
