407d23188c4ff6926d38f6b6fc1182244628ce04
Vitória Anjos <anjosvitoria80@gmail.com>
2026-04-08 22:52:11 -0300

    criando s3

--- src/main/resources/db/migration/V1__create-event-table.sql
+++ src/main/resources/db/migration/V1__create-event-table.sql
@@ -7,6 +7,5 @@ CREATE TABLE event (
     img_url VARCHAR(100) NOT NULL,
     event_url VARCHAR(100) NOT NULL,
     date TIMESTAMP NOT NULL,
-    remote BOOLEAN NOT NULL,
-
+    remote BOOLEAN NOT NULL
 );
